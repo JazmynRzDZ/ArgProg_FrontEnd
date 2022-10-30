8d05e2d442f3166f1794ef477f1a122547dba7ea
JazmynRzDZ <jazminrd@hotmail.es>
2022-10-30 01:32:08 -0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'jrdfrontend',
+    appId: '1:252737186980:web:b741ff0f23fad3f6d2ca31',
+    storageBucket: 'jrdfrontend.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDhWNIFy0fm0yqulBKcD478QIf4okXlDkU',
+    authDomain: 'jrdfrontend.firebaseapp.com',
+    messagingSenderId: '252737186980',
+    measurementId: 'G-508JLBKJZB',
+  },
+  production: true
+};
